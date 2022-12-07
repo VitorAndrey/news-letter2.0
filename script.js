ed894eaf494c41fd5ab5a50540d91ea361c5618e
VitorAndrey <vitor.andreylopess@gmail.com>
2022-12-07 12:03:46 -0300

    versao final 1 so falta quebrar as linhas dos paragrafos

---
+++ script.js
@@ -0,0 +1,16 @@
+const hamburguer = document.querySelector(".hamburguer");
+const navMenu = document.querySelector(".nav-menu");
+const opac = document.querySelector(".opac");
+
+
+hamburguer.addEventListener("click", () => {
+    hamburguer.classList.toggle("active")
+    navMenu.classList.toggle("active")
+    opac.classList.toggle("active")
+})
+
+document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () =>{
+    hamburguer.classList.remove("active")
+    navMenu.classList.remove("active")
+    opac.classList.remove("active")
+}))
